ea84ad7ec543c583a167750be56f002260b6fccf
jaejinee <012h21l@gmail.com>
2022-05-19 16:28:09 +0900

    deploying-3

--- src/db.js
+++ src/db.js
@@ -4,6 +4,6 @@ mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB!");
-const handleError = () => console.log("❌ DB Error", error);
+const handleError = (error) => console.log("❌ DB Error", error);
 db.on("error", handleError);
 db.once("open", handleOpen);
